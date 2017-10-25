50224d5e4177d22f1ecb4bf04cc9a784e908ec35
Jen Wachter <jenleighkelly@gmail.com>
2017-10-25 11:54:05 -0400

    Passed environmant in with config settings

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 /**
  * Create a new logger
  * @param  {string} dsn      Public DSN (found in Sentry)
+ * @param  {string} env      Environment
  * @param  {object} settings Optional config settings (https://docs.sentry.io/clients/javascript/config/)
  */
-var Logger = function (dsn, settings) {
+var Logger = function (dsn, env, settings) {
+
+  if (!dsn) throw new Error("DSN not provided to Logger object");
 
   settings = settings || {};
+  settings.environment = env;
+
   Raven.config(dsn, settings).install();
 
 };
